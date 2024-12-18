98bbc006c07abaec9bd5eb8baf9557f8b51c092d
Iman <bajalaniman@gmail.com>
2024-12-18 11:32:58 +0100

    changing in vite

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8800/";
+export const BASE_URL = "https://master-backend-production.up.railway.app/";
+// export const BASE_URL = "http://localhost:8800/";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
@@ -14,4 +13,7 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    sourcemap: true, // Enables source maps for debugging
+  },
 });
